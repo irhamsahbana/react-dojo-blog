01e82bf1faaa229cdf8ccbb014852e9dbef3ba02
irhamsahbana <irhamsahbana@gmail.com>
2021-10-03 13:15:21 +0800

    Function as props

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+const BlogList = (props) => {
+  return (
+    <div>
+      <div className="blog-list">
+        <h2>{props.title}</h2>
+        {props.blogs.map((blog) => (
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>written by {blog.author}</p>
+            <button onClick={() => props.handleDelete(blog.id)}>delete blog</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default BlogList
