6f18f5f4ec4fea7b3b866d12d4a177337231326a
irhamsahbana <irhamsahbana@gmail.com>
2021-10-05 13:24:46 +0800

    controlled Inputs (forms)

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,31 @@
+import { useState } from "react"
+
 const Create = () => {
-    return (
-        <div className="create">
-            <h2>Add a New Blog</h2>
-        </div>
-    )
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+  const [author, setAuthor] = useState('luigi')
+
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+
+      <form>
+        <label>Blog title:</label>
+        <input type="text" required value={title} onChange={e => setTitle(e.target.value)} />
+        <label>Blog body:</label>
+        <textarea required onChange={e => setBody(e.target.value)} value={body}></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={e => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="luigi">luigi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+      <p>{title}</p>
+      <p>{body}</p>
+      <p>{author}</p>
+    </div>
+  )
 }
 
 export default Create
